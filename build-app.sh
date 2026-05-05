193a5fd2538204ce6e03026224a950ca35588f47
Shuchen Zhao <shuchenzhao@shuchens-mac-studio.tail33b423.ts.net>
2026-05-04 22:17:16 -0700

    fix: expand app bundle version in build

--- build-app.sh
+++ build-app.sh
@@ -42,7 +42,7 @@ if [ -f "Assets/AppIcon.png" ]; then
 fi
 
 # Create Info.plist
-cat > TypeFish.app/Contents/Info.plist << 'PLIST'
+cat > TypeFish.app/Contents/Info.plist << PLIST
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
